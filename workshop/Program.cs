6dffe8b233b7cec3a44b0941359b4aee404666f3
Vasiliev Sergei <sergei.binp@gmail.com>
2023-04-27 23:17:25 +0500

    final commit: all tasks

--- workshop/Program.cs
+++ workshop/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 3. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+//A (3,6); B (2,1) -> 5,09
+//A (7,-5); B (1,-1) -> 7,21
+
+// double FindDistance(double x1, double y1, double x2, double y2){
+//     return Math.Round(Math.Sqrt(Math.Pow(y2 - y1, 2) + Math.Pow(x2 - x1, 2)), 2);
+// }
+
+// Console.WriteLine("Введите координаты двух точек: ");
+
+// Console.WriteLine("x1 = ");
+// double x1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("y1 = ");
+// double y1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("x2 = ");
+// double x2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("y2 = ");
+// double y2 = Convert.ToInt32(Console.ReadLine());
+
+// double res = FindDistance (x1, y1, x2, y2);
+// Console.WriteLine($"Расстояние между точками равняется: {res}");
+
+
+// //Задача 4. Напишите программу которая принимает на вход число (N) и выдает на консоль квадраты четных чисел от 1 до N
+// int i = 1;
+// Console.WriteLine("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// while (i <= n){
+//     if (i % 2 == 0) Console.WriteLine($"{(Math.Pow(i, 2))}, ");
+//     i++;
+// }
+
+
+// //Задача 5. Напишите программу которая принимает на вход число (N) и выдает на консоль квадраты чисел от 1 до N
+// int i = 1;
+// Console.WriteLine("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// while (i <= n){
+//     Console.WriteLine($"{(Math.Pow(i, 2))}, ");
+//     i++;
+// }
+
+
+//Задача 2.Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+// Console.WriteLine("Введите число: ");
