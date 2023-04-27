94a4f39f34095125533ed42e10890c5b6410551d
Vasiliev Sergei <sergei.binp@gmail.com>
2023-04-28 00:13:04 +0500

    2nd commit: second task

--- homework/Program.cs
+++ homework/Program.cs
@@ -10,6 +10,31 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+// Ввод пользователем координат двух точек
+Console.WriteLine("Введите координаты точки А: ");
+Console.WriteLine("x1 = ");
+double x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("y1 = ");
+double y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("z1 = ");
+double z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки В: ");
+Console.WriteLine("x2 = ");
+double x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("y2 = ");
+double y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("z1 = ");
+double z2 = Convert.ToInt32(Console.ReadLine());
+
+// Метод рассчёта расстояния между точками А и В
+double FindDistance(double x1, double y1, double z1, 
+                    double x2, double y2, double z2){
+                        return Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)), 2);
+                    }
+
+Console.WriteLine($"Расстояние между точками равно {FindDistance(x1, y1, z1, x2, y2, z2)}");
+
 
 // ************************************************************************************************************************************
 // Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
